298267048ece4b8c78a9526510b89b38201b218a
Tan Wen Hang Darrell <darrell.tan.2022@scis.smu.edu.sg>
2025-09-04 00:27:28 +0800

    configuring app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
+
 from src.routes.test_routes import test_route
 
 app = Flask(__name__,template_folder="src/templates")
-
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.register_blueprint(test_route)
 
 if __name__ == '__main__':
