775f4ac15c0aa09ee0c929e0f868958e15618990
Tan Wen Hang Darrell <darrell.tan.2022@scis.smu.edu.sg>
2025-09-05 15:17:02 +0800

    Create db connection and clean up main app.py

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import os
 
 from src.routes.test_routes import test_route
 
-app = Flask(__name__,template_folder="src/templates")
-load_dotenv()
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.register_blueprint(test_route)
+def create_app():
+    app = Flask(__name__,template_folder="src/templates")
+    load_dotenv()
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+    app.register_blueprint(test_route)
+    return app
 
 if __name__ == '__main__':
+    app=create_app()
     app.run(debug=True, host='0.0.0.0', port=5000)
