982c1e3af5f47274b45f8c301192c35403662522
Nichtea <107244767+Nichtea@users.noreply.github.com>
2025-10-29 21:21:57 +0800

    added missing imgs
    missing leaflet imgs

--- flood-viz/vite.config.ts
+++ flood-viz/vite.config.ts
@@ -2,9 +2,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
-
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),   viteStaticCopy({
+      targets: [
+        {
+          src: 'node_modules/leaflet/dist/images/*',  // all Leaflet images
+          dest: ''                                    // copy to dist root
+        }
+      ]
+    })],
   resolve: {
     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
